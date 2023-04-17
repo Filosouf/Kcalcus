34668d3eb7ff1d998665c0fe38a390d102949f6a
Soufian Boukaddour Hatlem <souhat@ihelse.net>
2023-04-17 08:52:48 +0200

    reDeplayed Angular for github Pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-kcalcus'`, () => {
+  it(`should have as title 'kcalcus'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-kcalcus');
+    expect(app.title).toEqual('kcalcus');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-kcalcus app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('kcalcus app is running!');
   });
 });
