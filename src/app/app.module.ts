7208d955a62633dd2eac43e245f179cf1a7e51ad
Soufian Boukaddour Hatlem <souhat@ihelse.net>
2023-04-17 12:44:03 +0200

    Prime theme added, styling and some convertion of function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputNumberModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
