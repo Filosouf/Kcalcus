f51721add75477be93e96eb9455aebf32da054ab
Soufian Boukaddour <48246091+Filosouf@users.noreply.github.com>
2023-04-18 14:38:45 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,30 +15,28 @@ export class AppComponent {
   private header: HTMLElement;
 
   constructor() {
-    this.input = document.getElementById('calories') as HTMLInputElement;
-    this.header = document.getElementById('Total') as HTMLElement;
+    // wait for PrimeNG components to render
+    setTimeout(() => {
+      this.input = document.getElementById('calories') as HTMLInputElement;
+      this.header = document.getElementById('Total') as HTMLElement;
+    }, 0);
   }
 
   onSave(): void {
-  const inputValue = Number(this.input.value);
+    const inputValue = Number(this.input.value);
 
-  // check if the input is a valid number
-  if (isNaN(inputValue)) {
-    alert('Please enter a valid number!');
-    return;
-  }
+    // check if the input is a valid number
+    if (isNaN(inputValue)) {
+      alert('Please enter a valid number!');
+      return;
+    }
 
-  // if total is 0, set total to input value, else add input value to total
-  if (this.total === 0) {
-    this.total = inputValue;
-  } else {
-    this.total += inputValue;
-  }
-
-  // replace header total with new total
-  this.header.innerText = this.total.toString();
-}
-
+    // if total is 0, set total to input value, else add input value to total
+    if (this.total === 0) {
+      this.total = inputValue;
+    } else {
+      this.total += inputValue;
+    }
 
     // replace header total with new total
     this.header.innerText = this.total.toString();
