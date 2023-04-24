2075bad50ae8e8bb128f8d2528b7322b84183e21
Soufian Boukaddour Hatlem <souhat@ihelse.net>
2023-04-24 14:10:55 +0200

    redone changes, staring over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,47 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public title = 'kcalcus';
-  
-  //store total for later use
-  public total: number = 0;
-  
-  private input: HTMLInputElement;
-  private header: HTMLElement;
-
-  constructor() {
-    // wait for PrimeNG components to render
-    setTimeout(() => {
-      this.input = document.getElementById('calories') as HTMLInputElement;
-      this.header = document.getElementById('Total') as HTMLElement;
-    }, 0);
-  }
-
-  onSave(): void {
-  const inputValue = parseFloat(this.input.value);
-
-  // check if the input is a valid number
-  if (isNaN(inputValue)) {
-    alert('Please enter a valid number!');
-    return;
-  }
-
-  // if total is 0, set total to input value, else add input value to total
-  if (this.total === 0) {
-    this.total = inputValue;
-  } else {
-    this.total += inputValue;
-  }
-  
-  // output debug information to the console
-  console.log(`Input value: ${inputValue}`);
-  console.log(`Current total: ${this.total}`);
-
-  // replace header total with new total
-  this.header.innerText = this.total.toString();
-}
-
-}
-
-
+  title = 'kcalcus';
 }
